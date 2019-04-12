24a82ca97f14f5a6242e1824f92803747f992a89
Daniel Dyrnes <thedanielforum@gmail.com>
2019-04-12 17:17:17 +0900

    Added example .env.yaml config

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-gcloud functions deploy cloud-build-notifications --entry-point receivedMessage --runtime nodejs8 --trigger-topic cloud-builds --project lesson-time-v4 --env-vars-file .env.yaml
+gcloud functions deploy cloud-build-notifications --entry-point receivedMessage --runtime nodejs8 --trigger-topic cloud-builds --project $PROJECT_ID --env-vars-file .env.yaml
